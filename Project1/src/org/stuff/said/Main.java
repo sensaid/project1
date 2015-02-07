dcfc90db65cdff14352f89847633df8d83180faf
bigdata06 <bigdata06@gmail.com>
2015-02-07 17:50:00 -0500

    first checkin projet1 said

---
+++ Project1/src/org/stuff/said/Main.java
@@ -0,0 +1,19 @@
+package org.stuff.said;
+
+import java.util.Date;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		displayDate();
+	}
+
+	/**
+	 * 
+	 */
+	private static void displayDate() {
+		System.out.println("Date today : "+new Date());
+	}
+
+}
